54917a54a51d3f12564c46fd971c56f99481c654
Berwin <Berwin.net@gmail.com>
2017-07-20 15:47:41 +0800

    s

--- src/com/mjoys/zjh/collect/TableCollector.java
+++ src/com/mjoys/zjh/collect/TableCollector.java
@@ -3,13 +3,13 @@ package com.mjoys.zjh.collect;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.mjoys.zjh.entity.Table;
+import com.mjoys.zjh.controller.TableController;
 
 public class TableCollector {
 
-	public static TableCollector instance = null;
+	private static TableCollector instance = null;
 
-	private List<Table> tables;
+	private List<TableController> tables;
 
 	private TableCollector() {
 		this.tables = new ArrayList<>();
@@ -22,11 +22,21 @@ public class TableCollector {
 		return instance;
 	}
 
-	public List<Table> getTables() {
+	/**
+	 * 加入或者创建一个房间/ 有可能是
+	 * 
+	 * @return
+	 */
+	public TableController quickStart() {
+
+		return null;
+	}
+
+	public List<TableController> getTables() {
 		return tables;
 	}
 
-	public void setTables(List<Table> tables) {
+	public void setTables(List<TableController> tables) {
 		this.tables = tables;
 	}
 
